a15f372c7c5c886380bbe9aece42e99d3d50556e
frankjimeuma <49537785+frankjimeuma@users.noreply.github.com>
2023-10-18 23:25:25 -0600

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -5,7 +5,22 @@ resource "aws_instance" "frank_instance" {
   key_name = aws_key_pair.frank_kp.key_name
   subnet_id = aws_subnet.subnet_frank_public.id
   vpc_security_group_ids = [aws_security_group.security_group04.id]
-
+  user_data = <<EOF
+#!/bin/bash
+sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install awscli -y
+sudo apt-get install nginx -y
+cd /var/www/
+sudo chgrp ubuntu html
+sudo chown ubuntu html
+wget https://iac-frank.s3.amazonaws.com/binaries.zip
+sudo apt install unzip
+unzip binaries.zip
+cd binaries
+cp /binaries/ /var/www/html/
+EOF
   tags = {
     Name = "PublicInstance"
   }
